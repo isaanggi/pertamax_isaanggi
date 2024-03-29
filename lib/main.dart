e73269dbbdbf0c1552a9b28569fb6b0ba5105323
isaanggi <isaanggi90@gmail.com>
2024-03-30 05:59:40 +0700

    Update Praktik Layout

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const String appTitle = 'Random Generate Word App';
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
-        title: 'Namer App',
+        title: appTitle,
         theme: ThemeData(
           useMaterial3: true,
           colorScheme:
-              ColorScheme.fromSeed(seedColor: Colors.green), // Mengganti warna
+              ColorScheme.fromSeed(seedColor: Colors.pink), // Mengganti warna
         ),
         home: MyHomePage(),
       ),
@@ -27,90 +28,238 @@ class MyApp extends StatelessWidget {
 }
 
 class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
+  var current = WordPair.random(); // Variabel kata saat ini
+  var history = <WordPair>[]; // Variabel history kata
 
-  // ↓ Memunculkan kata baru
+  GlobalKey? historyListKey;
+
+  // ↓ Memunculkan kata baru beserta history
   void getNext() {
+    history.insert(0, current);
+    var animatedList = historyListKey?.currentState as AnimatedListState?;
+    animatedList?.insertItem(0);
     current = WordPair.random();
     notifyListeners();
   }
 
-  var favorites = <WordPair>[];
+  var favorites = <WordPair>[]; // Variabel kata favorit
 
-  // ↓ Memfavorit kata
-  void toggleFavorite() {
-    if (favorites.contains(current)) {
-      favorites.remove(current);
+  // ↓ Action favorite kata
+  void toggleFavorite([WordPair? pair]) {
+    pair = pair ?? current;
+    if (favorites.contains(pair)) {
+      // ↓ Menghilangkan tanda favorite kata
+      favorites.remove([pair]);
     } else {
-      favorites.add(current);
+      // ↓ Memuncul tanda favorite kata
+      favorites.add(pair);
     }
+    notifyListeners();
+  }
+
+  // ↓ Action menghapus favorite kata
+  void removeFavorite(WordPair pair) {
+    favorites.remove(pair);
     notifyListeners();
   }
 }
 
+// ↓ Class untuk halaman generator word
 class MyHomePage extends StatefulWidget {
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-// ↓ Class yang muncul ketika StatefulWidget
 class _MyHomePageState extends State<MyHomePage> {
   var selectedIndex = 0; // Membuat variabel selectedIndex
+
   @override
   Widget build(BuildContext context) {
+    var colorScheme = Theme.of(context).colorScheme;
+
     Widget page;
     switch (selectedIndex) {
       case 0:
         page = GeneratorPage(); // Beralih ke halaman generator (Home)
-        break;
       case 1:
         page = FavoritesPage(); // Beralih ke halaman favorites
-        break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
     }
-    return LayoutBuilder(builder: (context, constraints) {
-      return Scaffold(
-        body: Row(
-          children: [
-            SafeArea(
-              child: NavigationRail(
-                extended: constraints.maxWidth >= 600, // Atur lebar responsive
-                destinations: [
-                  NavigationRailDestination(
-                    icon: Icon(Icons.home), // Material icon home
-                    label: Text('Home'), // Halaman Home
+
+    var mainArea = ColoredBox(
+      color: colorScheme.surfaceVariant,
+
+      // ↓ Animasi tiap ganti kata
+      child: AnimatedSwitcher(
+        duration: Duration(milliseconds: 200),
+        child: page,
+      ),
+    );
+
+    return Scaffold(
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth < 450) {
+            return Column(
+              children: [
+                Expanded(child: mainArea),
+                SafeArea(
+                  child: BottomNavigationBar(
+                    items: [
+                      BottomNavigationBarItem(
+                        icon: Icon(Icons.home), // Material icon home
+                        label: 'Home', // Label Halaman Home
+                      ),
+                      BottomNavigationBarItem(
+                        icon: Icon(Icons.favorite), // Material icon favorites
+                        label: 'Favorites', // Label Halaman Favorites
+                      ),
+                    ],
+                    currentIndex:
+                        selectedIndex, // Koneksi ke variabel selectedIndex
+                    onTap: (value) {
+                      setState(() {
+                        selectedIndex = value; // Memposisikan ke selectedIndex
+                      });
+                    },
                   ),
-                  NavigationRailDestination(
-                    icon: Icon(Icons.favorite), // Material icon favorites
-                    label: Text('Favorites'), // Halaman Favorites
+                )
+              ],
+            );
+          } else {
+            return Row(
+              children: [
+                SafeArea(
+                  child: NavigationRail(
+                    extended:
+                        constraints.maxWidth >= 600, // Atur lebar responsive
+                    destinations: [
+                      NavigationRailDestination(
+                        icon: Icon(Icons.home), // Material icon home
+                        label: Text('Home'), // Label Halaman Home
+                      ),
+                      NavigationRailDestination(
+                        icon: Icon(Icons.favorite), // Material icon favorites
+                        label: Text('Favorites'), // Label Halaman Favorites
+                      ),
+                    ],
+                    selectedIndex:
+                        selectedIndex, // Koneksi ke variabel selectedIndex
+                    onDestinationSelected: (value) {
+                      setState(() {
+                        selectedIndex = value; // Memposisikan ke selectedIndex
+                      });
+                    },
                   ),
-                ],
-                selectedIndex:
-                    selectedIndex, // Koneksi ke variabel selectedIndex
-                onDestinationSelected: (value) {
-                  setState(() {
-                    selectedIndex = value; // Memposisikan ke selectedIndex
-                  });
+                ),
+                Expanded(child: mainArea),
+              ],
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+// Widget gambar
+class ImageWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SizedBox(width: 40);
+    return Image.asset(
+      'images/utdi.png',
+      width: 150,
+      height: 150,
+    );
+  }
+}
+
+// Widget text
+class TextWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            '215411037',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            'Isa Anggie Alfianto',
+            style: TextStyle(fontSize: 16),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// ↓ Class untuk memunculkan history kata
+class HistoryListView extends StatefulWidget {
+  const HistoryListView({Key? key}) : super(key: key);
+
+  @override
+  State<HistoryListView> createState() => _HistoryListViewState();
+}
+
+class _HistoryListViewState extends State<HistoryListView> {
+  final _key = GlobalKey();
+
+  // Untuk fade out history
+  static const Gradient _maskingGradient = LinearGradient(
+    // Gradiasi warna history
+    colors: [Colors.transparent, Colors.black],
+    stops: [0.0, 0.5],
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    final appState = context.watch<MyAppState>();
+    appState.historyListKey = _key;
+
+    return ShaderMask(
+      shaderCallback: (bounds) => _maskingGradient.createShader(bounds),
+      blendMode: BlendMode.dstIn,
+      child: AnimatedList(
+        key: _key,
+        reverse: true,
+        padding: EdgeInsets.only(top: 100),
+        initialItemCount: appState.history.length,
+        itemBuilder: (context, index, animation) {
+          final pair = appState.history[index];
+          return SizeTransition(
+            sizeFactor: animation,
+            child: Center(
+              child: TextButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite(pair);
                 },
+                icon: appState.favorites.contains(pair)
+                    ? Icon(Icons.favorite, size: 12)
+                    : SizedBox(),
+                label: Text(
+                  pair.asLowerCase,
+                  semanticsLabel: pair.asPascalCase,
+                ),
               ),
             ),
-            Expanded(
-              child: Container(
-                color: Theme.of(context).colorScheme.primaryContainer,
-                child: page, // ← Halaman baru
-              ),
-            ),
-          ],
-        ),
-      );
-    });
+          );
+        },
+      ),
+    );
   }
 }
 
 class FavoritesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var theme = Theme.of(context);
     var appState = context.watch<MyAppState>();
 
     if (appState.favorites.isEmpty) {
@@ -119,18 +268,38 @@ class FavoritesPage extends StatelessWidget {
       );
     }
 
-    return ListView(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Padding(
-          padding: const EdgeInsets.all(20),
-          child: Text('You have '
-              '${appState.favorites.length} favorites:'),
+          padding: const EdgeInsets.all(30),
+          child: Text('Kamu punya '
+              '${appState.favorites.length} kata favorit:'),
         ),
-        for (var pair in appState.favorites)
-          ListTile(
-            leading: Icon(Icons.favorite),
-            title: Text(pair.asLowerCase),
+        Expanded(
+          child: GridView(
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 400,
+              childAspectRatio: 400 / 80,
+            ),
+            children: [
+              for (var pair in appState.favorites)
+                ListTile(
+                  leading: IconButton(
+                    icon: Icon(Icons.delete_outline, semanticLabel: 'Delete'),
+                    color: theme.colorScheme.primary,
+                    onPressed: () {
+                      appState.removeFavorite(pair);
+                    },
+                  ),
+                  title: Text(
+                    pair.asLowerCase,
+                    semanticsLabel: pair.asPascalCase,
+                  ),
+                ),
+            ],
           ),
+        ),
       ],
     );
   }
@@ -145,17 +314,22 @@ class GeneratorPage extends StatelessWidget {
     // ↓ Menambah icon
     IconData icon;
     if (appState.favorites.contains(pair)) {
-      icon = Icons.favorite; // Material icon favorites
+      icon = Icons.favorite; // Material icon favorites nyala
     } else {
-      icon = Icons.favorite_border;
+      icon = Icons.favorite_border; // Material icon favorites mati
     }
 
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center, // Merubah posisi ke center
         children: [
+          Expanded(
+            flex: 3,
+            child: HistoryListView(),
+          ),
+          SizedBox(height: 30), // Buat gaps antara history dan current word
           BigCard(pair: pair), // Membuat class BigCard
-          SizedBox(height: 30), // Buat gaps antara widget BigCard dan Button
+          SizedBox(height: 10), // Buat gaps antara widget BigCard dan Button
           // ↓ Menambah tombol
           Row(
             mainAxisSize: MainAxisSize.min,
@@ -168,15 +342,32 @@ class GeneratorPage extends StatelessWidget {
                 icon: Icon(icon), // ← Koneksi ke icon
                 label: Text('Like'),
               ),
-              SizedBox(width: 30), // Gaps antar tombol
-              ElevatedButton(
+              SizedBox(width: 10), // Gaps antar tombol
+              ElevatedButton.icon(
                 onPressed: () {
                   appState.getNext(); // ← Koneksi ke MyAppState
                 },
-                child: Text('Next'),
+                icon: Icon(icon = Icons.arrow_forward),
+                label: Text('Next'),
               ),
             ],
           ),
+          SizedBox(height: 20), // Spacer
+          Row(
+            children: [
+              // Image widget
+              Expanded(
+                flex: 1,
+                child: ImageWidget(),
+              ),
+              // Text widget
+              Expanded(
+                flex: 1,
+                child: TextWidget(),
+              ),
+            ],
+          ),
+          SizedBox(height: 20), // Spacer
         ],
       ),
     );
@@ -185,17 +376,17 @@ class GeneratorPage extends StatelessWidget {
 
 class BigCard extends StatelessWidget {
   const BigCard({
-    super.key,
+    Key? key,
     required this.pair,
-  });
+  }) : super(key: key);
 
   final WordPair pair;
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context); // Menambah tema
+    var theme = Theme.of(context); // Menambah tema
     // ↓ Gaya tema
-    final style = theme.textTheme.displayMedium!.copyWith(
+    var style = theme.textTheme.displayMedium!.copyWith(
       color: theme.colorScheme.onPrimary,
     );
 
@@ -203,10 +394,22 @@ class BigCard extends StatelessWidget {
       color: theme.colorScheme.primary, // Memberi warna
       child: Padding(
         padding: const EdgeInsets.all(20), // Ukuran padding
-        child: Text(
-          pair.asLowerCase,
-          style: style,
-          semanticsLabel: "${pair.first} ${pair.second}", // Override visual
+        child: AnimatedSize(
+          duration: Duration(milliseconds: 200),
+          child: MergeSemantics(
+            child: Wrap(
+              children: [
+                Text(
+                  pair.first,
+                  style: style.copyWith(fontWeight: FontWeight.w200),
+                ),
+                Text(
+                  pair.second,
+                  style: style.copyWith(fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
